b677dfcdc7ba6ccf13acedea83570fe8608846fa
Ivan Cerrato <ivan@cerrato.biz>
2013-12-20 02:24:44 +0100

    cpu-tegra: fix max suspend cpu frequency

--- arch/arm/mach-tegra/cpu-tegra.c
+++ arch/arm/mach-tegra/cpu-tegra.c
@@ -636,39 +636,41 @@ _out:
 
 	return ret;
 }
-
+#ifdef CONFIG_KOWALSKI_CPU_SUSPEND_FREQ_LIMIT
+        extern unsigned int kowalski_cpu_suspend_max_freq;
+        static unsigned int stored_cpu_user_cap = 0;
+#endif
 
 static int tegra_pm_notify(struct notifier_block *nb, unsigned long event,
 	void *dummy)
 {
-#ifdef CONFIG_KOWALSKI_CPU_SUSPEND_FREQ_LIMIT
-	extern unsigned int kowalski_cpu_suspend_max_freq;
-	unsigned int stored_cpu_user_cap = 0;
-#endif
 	mutex_lock(&tegra_cpu_lock);
 	if (event == PM_SUSPEND_PREPARE) {
+#ifdef CONFIG_KOWALSKI_CPU_SUSPEND_FREQ_LIMIT
+		if (kowalski_cpu_suspend_max_freq) {
+			pr_info("Kowalski cpufreq suspend: setting max frequency to %d kHz\n", kowalski_cpu_suspend_max_freq);
+			if (cpu_user_cap)
+				stored_cpu_user_cap = cpu_user_cap;
+			else
+				stored_cpu_user_cap = policy_max_speed[0];
+			cpu_user_cap = kowalski_cpu_suspend_max_freq;
+		}
+#endif
 		is_suspended = true;
 		pr_info("Tegra cpufreq suspend: setting frequency to %d kHz\n",
 			freq_table[suspend_index].frequency);
 		tegra_update_cpu_speed(freq_table[suspend_index].frequency);
 		tegra_auto_hotplug_governor(
 			freq_table[suspend_index].frequency, true);
-#ifdef CONFIG_KOWALSKI_CPU_SUSPEND_FREQ_LIMIT
-		if (kowalski_cpu_suspend_max_freq) {
-			pr_info("Kowalski cpufreq suspend: setting max frequency to %d kHz\n", kowalski_cpu_suspend_max_freq);
-			stored_cpu_user_cap = cpu_user_cap;
-			cpu_user_cap = kowalski_cpu_suspend_max_freq;
-		}
-#endif
 	} else if (event == PM_POST_SUSPEND) {
+		unsigned int freq;
 #ifdef CONFIG_KOWALSKI_CPU_SUSPEND_FREQ_LIMIT
 		if (stored_cpu_user_cap) {
-		pr_info("Kowalski cpufreq resume: restoring max frequency to %d kHz\n", stored_cpu_user_cap);
-		cpu_user_cap = stored_cpu_user_cap;
-		stored_cpu_user_cap = 0;
-}
+			pr_info("Kowalski cpufreq resume: restoring max frequency to %d kHz\n", stored_cpu_user_cap);
+			cpu_user_cap = stored_cpu_user_cap;
+			stored_cpu_user_cap = 0;
+		}
 #endif
-		unsigned int freq;
 		is_suspended = false;
 		tegra_cpu_edp_init(true);
 		tegra_cpu_set_speed_cap(&freq);
